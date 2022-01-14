26da96da84a11760c71d81abc1646051702c45d4
Caesar <pepijndekeijzer@gmail.com>
2022-01-14 22:35:07 +0100

    update

--- js/cv.js
+++ js/cv.js
@@ -1,9 +1,9 @@
-function passwd(){
+function pwdCVprotect(){
 
   var password = prompt('Enter the password to download Curriculum Vitae file! By uncertainty, please email me at: Pepijndekeijzer@gmail.com');
   if(password.toLowerCase() == "found_on_github!"){
     window.open("images/myCV.pdf")
   }else{
-    alert("incorrect password!! please try again");
+    alert(" ai that is a shame, incorrect password! please try again");
   }
 }
