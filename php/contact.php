2cf1137012af01cb509884dc77e7755c1ef05265
Caesar <pepijndekeijzer@gmail.com>
2022-01-15 00:15:11 +0100

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "pepijndekeijzer@gmail.com";
+$address = "Pepijndekeijzer@gmail.com";
 
 
 // Configuration option.
