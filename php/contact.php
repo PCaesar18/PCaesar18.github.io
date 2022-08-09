bebd20bcfc460938967a3beb09f84a5954e06e8a
Caesar <pepijndekeijzer@gmail.com>
2022-08-09 08:06:03 +0200

    contact form update

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "pepijndk@icloud.com";
+$address = "pepijndekeijzer@gmail.com";
 
 
 // Configuration option.
