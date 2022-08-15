56db88f9b6384e7ee80d903993cc6531bfa211ef
Caesar <pepijndekeijzer@gmail.com>
2022-08-15 18:49:52 +1000

    buttons upon btns

--- js/cv.js
+++ js/cv.js
@@ -7,3 +7,15 @@ function pwdCVprotect(){
     alert(" ai that is a shame, incorrect password! please try again");
   }
 }
+
+function openThesis(){
+  window.open("images/Thesis.pdf")
+  //
+  // var password = prompt('Enter the password to download Curriculum Vitae file! By uncertainty, please email me at: Pepijndekeijzer@gmail.com');
+  // if(password.toLowerCase() == "found_on_github!"){
+  //   window.open("images/myCV.pdf")
+  // }else{
+  //   alert(" ai that is a shame, incorrect password! please try again");
+  // }
+}
+
